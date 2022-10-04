f0f1599ad4390575df31826a63c515217d5c9a63
Van-Ivan <pibo0121016@sibupk.su>
2022-10-04 21:36:50 +0700

    Выгружаю код

---
+++ Проверка високосности 1/Проверка високосности 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_на_високосность_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 4 == 0)
+                    Console.WriteLine("Високосный");
+            else
+                Console.WriteLine("Не високосный");
+            Console.ReadLine();
+        }
+    }
+}
